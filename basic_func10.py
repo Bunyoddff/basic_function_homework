b8d272e1cc770c9c19ffe141f6a4f14ba28e058c
Bunyoddff <bkhusanov444@gmail.com>
2022-12-27 10:54:25 +0500

    pyo10

--- basic_func10.py
+++ basic_func10.py
@@ -1,6 +1,7 @@
 # Create a function called main.
-def main(a)
+def main(a):
 # Create function arguments a.
-    a=-a
+    a=a*(-1)
     return a
 # Return the opposite value of a.
+#print(main(45))
